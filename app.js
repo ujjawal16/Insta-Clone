040027bb1ee95a26935b414c39733dc1cef138b2
ujjawal16 <ujjawal.golani@gmail.com>
2021-05-22 11:22:23 +0530

    final

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app=express()
-const PORT=process.env.PORT||5000
+const PORT=process.env.PORT ||5000
 const mongoose=require('mongoose')
 const { MONGOURI } = require('./config/keys')
 
